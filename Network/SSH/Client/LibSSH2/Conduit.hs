81c4e521ee1449f271cea0215863a0869fb576bf
Tim Dysinger <tim@dysinger.net>
2012-06-27 17:41:16 -1000

    Rework code to work with Conduit 0.4.x

--- Network/SSH/Client/LibSSH2/Conduit.hs
+++ Network/SSH/Client/LibSSH2/Conduit.hs
@@ -1,90 +1,74 @@
 {-# LANGUAGE FlexibleContexts #-}
+
 module Network.SSH.Client.LibSSH2.Conduit
-  (sourceChannel,
-   splitLines,
-   CommandsHandle,
-   execCommand,
-   getReturnCode
-  ) where
+       ( CommandsHandle
+       , execCommand
+       , getReturnCode
+       , sourceChannel
+       , splitLines
+       ) where
 
-import Control.Monad
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Resource
-import Control.Monad.Trans.Control
-import System.IO.Unsafe (unsafeInterleaveIO)
 import Control.Concurrent.STM
-import Data.Monoid
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Resource
 import Data.Conduit
-import Data.Conduit.Lazy
-
-import Network.SSH.Client.LibSSH2.Foreign
 import Network.SSH.Client.LibSSH2
+import Network.SSH.Client.LibSSH2.Foreign
 
 -- | Read all contents of libssh2's Channel.
-sourceChannel :: Channel -> Source IO String
+sourceChannel :: MonadResource m => Channel -> Source m String
 sourceChannel ch = src
   where
-    src = Source pull close
-
-    pull = do
-        (sz, res) <- liftIO $ readChannel ch 0x400
-        if sz > 0
-            then return $ Open src res
-            else return Closed
-
-    close = return ()
+    src = sourceIO initState defaultClose pull
+    pull _state = do
+      (sz, res) <- liftIO $ readChannel ch 0x400
+      if sz > 0
+        then return (IOOpen res)
+        else return IOClosed
 
 -- | Similar to Data.Conduit.Binary.lines, but for Strings.
-splitLines :: Resource m => Conduit String m String
+splitLines :: MonadResource m => Conduit String m String
 splitLines =
-    conduitState id push close
+  conduitState id push close
   where
     push front bs' = return $ StateProducing leftover ls
       where
         bs = front bs'
         (leftover, ls) = getLines id bs
-
     getLines front bs
         | null bs = (id, front [])
         | null y = ((x ++), front [])
         | otherwise = getLines (front . (x:)) (drop 1 y)
       where
         (x, y) = break (== '\n') bs
-
     close front
         | null bs = return []
         | otherwise = return [bs]
       where
         bs = front ""
 
--- | Execute one command and read it's output lazily.
--- If first argument is True, then you *must* get return code
--- using getReturnCode on returned CommandsHandle. Moreover,
--- you *must* guarantee that getReturnCode will be called
--- only when all command output will be read.
-execCommand :: Bool                          -- ^ Set to True if you want to get return code when command will terminate.
-            -> Session
-            -> String                        -- ^ Command
-            -> IO (Maybe CommandsHandle, Source IO String) 
-execCommand b s cmd = do
-  (ch, channel) <- initCH b s
+-- | Execute one command and read it's output lazily.  If first
+-- argument is True, then you *must* get return code using
+-- getReturnCode on returned CommandsHandle. Moreover, you *must*
+-- guarantee that getReturnCode will be called only when all command
+-- output will be read.
+execCommand ::
+  MonadResource m
+  => Bool
+  -> Session
+  -> String
+  -> IO (Maybe CommandsHandle, Source m String)
+execCommand returnCodeOnExit sess cmd = do
+  (ch, channel) <- initCH returnCodeOnExit sess
   let src = execCommandS ch channel cmd $= splitLines
-  return (if b then Just ch else Nothing, src)
-
--- execCommands :: Bool -> Session -> [String] -> IO [String]
--- execCommands b s cmds = do
---   let srcs = [execCommandS (v i) s cmd | (i, cmd) <- zip [1..] cmds ]
---       v i | i == length cmds = var
---           | otherwise        = Nothing
---   res <- runResourceT $ lazyConsume $ mconcat srcs $= splitLines
---   return res
+  return (if returnCodeOnExit then Just ch else Nothing, src)
 
 -- | Handles channel opening and closing.
-data CommandsHandle = CommandsHandle {
-  chReturnCode :: Maybe (TMVar Int),
-  chChannel :: TMVar Channel,
-  chChannelClosed :: TVar Bool }
+data CommandsHandle =
+  CommandsHandle { chReturnCode    :: Maybe (TMVar Int),
+                   chChannel       :: TMVar Channel,
+                   chChannelClosed :: TVar Bool }
 
 initCH :: Bool -> Session -> IO (CommandsHandle, Channel)
 initCH False s = do
@@ -101,20 +85,21 @@ initCH True s = do
 
 openCH :: TMVar Channel -> Session -> IO Channel
 openCH var s = do
-      ch <- openChannelSession s
-      atomically $ putTMVar var ch
-      return ch
+  ch <- openChannelSession s
+  atomically $ putTMVar var ch
+  return ch
 
--- | Get return code for previously run command.
--- It will fail if command was run using execCommand False.
--- Should be called only when all command output is read.
+-- | Get return code for previously run command.  It will fail if
+-- command was run using execCommand False.  Should be called only
+-- when all command output is read.
 getReturnCode :: CommandsHandle -> IO Int
 getReturnCode ch = do
   c <- atomically $ readTVar (chChannelClosed ch)
   if c
     then do
       case chReturnCode ch of
-        Nothing -> fail "Channel already closed and no exit code return was set up for command."
+        Nothing -> fail $ "Channel already closed and no exit"
+                   ++ " code return was set up for command."
         Just v -> atomically $ takeTMVar v
     else do
       channel <- atomically $ takeTMVar (chChannel ch)
@@ -122,33 +107,29 @@ getReturnCode ch = do
       atomically $ writeTVar (chChannelClosed ch) True
       case chReturnCode ch of
         Nothing -> fail "No exit code return was set up for commnand."
-        Just v  -> do
-                   rc <- atomically $ takeTMVar v
-                   return rc
-    
-execCommandS :: CommandsHandle -> Channel -> String -> Source IO String
-execCommandS var channel command =
-  Source {
-      sourcePull = pull channel 
-    , sourceClose = return () }
-  where
-    
-    next ch =
-      Source (pullAnswer ch) $ do
-          return ()
-          --liftIO $ cleanupChannel var ch
+        Just v  -> do rc <- atomically $ takeTMVar v
+                      return rc
 
-    pullAnswer ch = do
+execCommandS ::
+  MonadResource m
+  => CommandsHandle
+  -> Channel
+  -> String
+  -> Source m String
+execCommandS var channel command =
+  source (pull channel)
+  where
+    source = sourceIO initState defaultClose
+    next ch = source (pullAnswer ch)
+    pullAnswer ch _state = do
       (sz, res) <- liftIO $ readChannel ch 0x400
       if sz > 0
-        then return $ Open (next ch) res
-        else do
-             liftIO $ cleanupChannel var ch
-             return Closed
-
-    pull ch = do
+        then return (IOOpen res)
+        else do liftIO $ cleanupChannel var ch
+                return IOClosed
+    pull ch state = do
       liftIO $ channelExecute ch command
-      pullAnswer ch
+      pullAnswer ch state
 
 -- | Close Channel and write return code
 cleanupChannel :: CommandsHandle -> Channel -> IO ()
@@ -166,3 +147,8 @@ cleanupChannel ch channel = do
     atomically $ writeTVar (chChannelClosed ch) True
     return ()
 
+initState :: IO String
+initState  = return ""
+
+defaultClose :: a -> IO ()
+defaultClose _ = return ()
